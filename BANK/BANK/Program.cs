e452705bf98bd1b6dc5b4e07eb7516a19c897a67
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-03 00:45:23 +0600

    Program class work started

--- BANK/BANK/Program.cs
+++ BANK/BANK/Program.cs
@@ -10,6 +10,34 @@ namespace BANK
     {
         static void Main(string[] args)
         {
-        }
+            bool cycle = true;
+            Bank bank = new Bank();
+            bank.BankName = "Stay_Humble";
+            Console.WriteLine("Welcome To {0} Bank", bank.BankName);
+            while (cycle)
+            {
+                Console.WriteLine("Select From-");
+                Console.WriteLine("1.Add Account\n2.Delete Account\n3.Transaction\n4.Print Account Details\n5.Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Account ac = new Account(new Address());
+                        Console.WriteLine("Enter Your Account Name :");
+                        ac.AccountName = Console.ReadLine();
+                        Console.WriteLine("Enter Balance :");
+                        ac.Balance = Convert.ToInt32(Console.ReadLine());
+                        bank.AddAccount(ac);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Enter Account Number To Delete :");
+                        int Nd = Convert.ToInt32(Console.ReadLine());
+                        bank.DeleteAccount(Nd);
+                        break;
+
+
+
+                }
     }
 }
