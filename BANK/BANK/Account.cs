4c7ae83d859e7f835a0efd995f7c9d77b899f6e9
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-03 00:10:02 +0600

    Bank And Account Class Partially Done

--- BANK/BANK/Account.cs
+++ BANK/BANK/Account.cs
@@ -13,5 +13,45 @@ namespace BANK
         private double balance;
         private Address address;
         static int idg = 5670;
+
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+
+        public Address Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance = balance - amount;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance = balance + amount;
+            }
+        }
+
     }
 }
--- BANK/BANK/Bank.cs
+++ BANK/BANK/Bank.cs
@@ -10,5 +10,51 @@ namespace BANK
     {
         private string bankName;
         private Account[] myBank = new Account[5];
+
+        public void AddAccount(Account account)
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i] == null)
+                {
+                    myBank[i] = account;
+                    myBank[i].GenarateAccountNumber();
+                    Console.WriteLine("Enter Your Address_");
+                    Console.WriteLine("Road No, House No, City, Country -");
+                    myBank[i].Address.RoadNo = Console.ReadLine();
+                    myBank[i].Address.HouseNo = Console.ReadLine();
+                    myBank[i].Address.City = Console.ReadLine();
+                    myBank[i].Address.Country = Console.ReadLine();
+                    Console.WriteLine("New Account Created..");
+                    myBank[i].ShowAccountInformation();
+                    break;
+                }
+            }
+        }
+
+        public void DeleteAccount(int accountNumber)
+        {
+            for (int i = 0; i < myBank.Length; i++)
+            {
+                if (myBank[i].AccountNumber == accountNumber)
+                {
+                    myBank[i] = null;
+                    Console.WriteLine("Account Deleted..");
+                    break;
+                }
+            }
+
+        }
+
+        public void PrintAccountDetails()
+        {
+            for (int i = 0; myBank[i] != null; i++)
+            {
+                Console.WriteLine((i + 1) + ". " + myBank[i].AccountName);
+            }
+            Console.WriteLine("Insert Account Serial No. If You Further Want To See The Full Information Of An Account...");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            myBank[choice - 1].ShowAccountInformation();
+        }
     }
 }
