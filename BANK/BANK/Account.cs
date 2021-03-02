21fd64472c51e74d850ff8255445007b82da7264
17-34928-2@student.aiub.edu <17-34928-2@student.aiub.edu>
2021-03-02 23:53:54 +0600

    Project start

---
+++ BANK/BANK/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private Address address;
+        static int idg = 5670;
+    }
+}
---
+++ BANK/BANK/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK
+{
+    class Address
+    {
+        private string roadNo;
+        private string houseNo;
+        private string city;
+        private string country;
+    }
+}
